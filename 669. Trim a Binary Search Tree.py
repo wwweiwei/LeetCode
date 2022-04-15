21f62da0555b325bfbcb44cd4c41d3388653bab2
Wei-Wei Du <44032506+wwweiwei@users.noreply.github.com>
2022-04-16 00:02:49 +0800

    Apr. 16

--- 669. Trim a Binary Search Tree.py
+++ 669. Trim a Binary Search Tree.py
@@ -6,14 +6,30 @@
 #         self.right = right
 class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        if root.val > high:
-            return self.trimBST(root.left, low, high)
-        elif root.val < low:
-            return self.trimBST(root.right, low, high)
-        else:
-            root.left = self.trimBST(root.left, low, high)
-            root.right = self.trimBST(root.right, low, high)
-            return root
+        def check(root):
+            if not root:
+                return None
+            if root.val > high:
+                root = root.left
+                return check(root)
+            elif root.val < low:
+                root = root.right
+                return check(root)
+            else:
+                root.left = check(root.left)
+                root.right = check(root.right)
+                return root
+                
+        return check(root)  
+        
+        # if not root:
+        #     return None
+        # if root.val > high:
+        #     return self.trimBST(root.left, low, high)
+        # elif root.val < low:
+        #     return self.trimBST(root.right, low, high)
+        # else:
+        #     root.left = self.trimBST(root.left, low, high)
+        #     root.right = self.trimBST(root.right, low, high)
+        #     return root
         
